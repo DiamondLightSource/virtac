4b4394a206107c567253b4f82fafcac02bf160d4
Phil Smith <pts@observatorysciences.co.uk>
2025-06-27 16:55:41 +0100

    Remove atip tests
    Also update test_cli.py to point to virtac

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,9 @@
 import subprocess
 import sys
 
-from atip import __version__
+from virtac import __version__
 
 
 def test_cli_version():
-    cmd = [sys.executable, "-m", "atip", "--version"]
+    cmd = [sys.executable, "-m", "virtac", "--version"]
     assert subprocess.check_output(cmd).decode().strip() == __version__
