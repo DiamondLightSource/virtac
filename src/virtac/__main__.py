c32ffb6f7c31dc005defe0bfbf976ef34e4a914d
Phil Smith <pts@observatorysciences.co.uk>
2025-08-04 13:50:33 +0000

    Refactor ca config into its own func

--- src/virtac/__main__.py
+++ src/virtac/__main__.py
@@ -52,11 +52,54 @@ def parse_arguments():
     return parser.parse_args()
 
 
+def configure_ca():
+    """Setup channel access settings for our CA server and for accessing PVs
+    from other IOCs. We will be creating a python softioc IOC which automatically
+    creates a CA server to serve our PVs.
+    """
+
+    # Warn if set to default EPICS port(s) as this will likely cause PV conflicts.
+    conflict_warning = ", this may lead to conflicting PV names with production IOCs."
+    epics_env_vars = [
+        "EPICS_CA_REPEATER_PORT",
+        "EPICS_CAS_SERVER_PORT",
+        "EPICS_CA_SERVER_PORT",
+        "EPICS_CAS_BEACON_PORT",
+    ]
+    ports_list = [int(os.environ.get(env_var, 0)) for env_var in epics_env_vars]
+    if 5064 in ports_list or 5065 in ports_list:
+        warn(
+            f"At least one of {epics_env_vars} is set to 5064 or 5065"
+            + conflict_warning,
+            stacklevel=1,
+        )
+    elif all(port == 0 for port in ports_list):
+        warn(
+            "No EPICS port set, default base port (5064) will be used"
+            + conflict_warning,
+            stacklevel=1,
+        )
+
+    # Avoid PV conflict between multiple IP interfaces on the same machine.
+    primary_ip = socket.gethostbyname(socket.getfqdn())
+    if "EPICS_CAS_INTF_ADDR_LIST" in os.environ.keys():
+        warn(
+            "Pre-existing 'EPICS_CAS_INTF_ADDR_LIST' value" + conflict_warning,
+            stacklevel=1,
+        )
+    else:
+        os.environ["EPICS_CAS_INTF_ADDR_LIST"] = primary_ip
+        os.environ["EPICS_CAS_BEACON_ADDR_LIST"] = primary_ip
+        os.environ["EPICS_CAS_AUTO_BEACON_ADDR_LIST"] = "NO"
+
+
 def main():
     """Main entrypoint for virtac. Executed when running the 'virtac' command"""
     args = parse_arguments()
     log_level = logging.DEBUG if args.verbose else logging.INFO
     logging.basicConfig(level=log_level, format=LOG_FORMAT)
+
+    configure_ca()
 
     # Determine the ring mode
     if args.ring_mode is not None:
@@ -87,39 +130,6 @@ def main():
         DATADIR / ring_mode / "tunefb.csv",
         args.disable_emittance,
     )
-
-    # Warn if set to default EPICS port(s) as this will likely cause PV conflicts.
-    conflict_warning = ", this may lead to conflicting PV names with production IOCs."
-    epics_env_vars = [
-        "EPICS_CA_REPEATER_PORT",
-        "EPICS_CAS_SERVER_PORT",
-        "EPICS_CA_SERVER_PORT",
-        "EPICS_CAS_BEACON_PORT",
-    ]
-    ports_list = [int(os.environ.get(env_var, 0)) for env_var in epics_env_vars]
-    if 5064 in ports_list or 5065 in ports_list:
-        warn(
-            f"At least one of {epics_env_vars} is set to 5064 or 5065"
-            + conflict_warning,
-            stacklevel=1,
-        )
-    elif all(port == 0 for port in ports_list):
-        warn(
-            "No EPICS port set, default base port (5064) will be used"
-            + conflict_warning,
-            stacklevel=1,
-        )
-    # Avoid PV conflict between multiple IP interfaces on the same machine.
-    primary_ip = socket.gethostbyname(socket.getfqdn())
-    if "EPICS_CAS_INTF_ADDR_LIST" in os.environ.keys():
-        warn(
-            "Pre-existing 'EPICS_CAS_INTF_ADDR_LIST' value" + conflict_warning,
-            stacklevel=1,
-        )
-    else:
-        os.environ["EPICS_CAS_INTF_ADDR_LIST"] = primary_ip
-        os.environ["EPICS_CAS_BEACON_ADDR_LIST"] = primary_ip
-        os.environ["EPICS_CAS_AUTO_BEACON_ADDR_LIST"] = "NO"
 
     # Start the IOC.
     builder.LoadDatabase()
